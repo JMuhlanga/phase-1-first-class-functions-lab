43fe1ce981fde319c60aeed403fa7da98315f7d3
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-20 19:43:51 +0300

    Completed on Aug 20

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (someArray) => { 
+    return someArray.slice(0,2);
+}
+
+const returnLastTwoDrivers = (someArray) => {
+    return someArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = (someValue1) => {
+    return function(someValue2){
+        return someValue1 * someValue2 ; 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (someArray , someFunction) => {
+    return someFunction(someArray);
+}
+
+
+
+
